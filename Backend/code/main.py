f852b9fe2d624a5e7fea6481ca3b2e630ab17f79
Coachmeister <jolun18@student.sdu.dk>
2019-12-04 15:44:31 +0100

    ..

--- Backend/code/main.py
+++ Backend/code/main.py
@@ -1,15 +1,20 @@
 from flask import Flask, redirect, url_for, request, jsonify
-
+from mysql.connector import (connection)
 app = Flask(__name__)
 
+cnx = connection.MySQLConnection(user='Jonas', password='password',
+                                 host='localhost',
+								database='people')
+cnx.close()
 
-@app.route('/person', methods = ['POST'])
+
+@app.route('/person/', methods = ['POST'])
 def post():
 	firstname = request.form['firstname']
 	lastname = request.form['lastname']
 	submit = request.form['submit']
 
-@app.route('/person', methods = ['GET'])
+@app.route('/persons/', methods = ['GET'])
 def get():
 	return jsonify(PersonID = 0, firstname ="fuck", lastname = "you")
 
