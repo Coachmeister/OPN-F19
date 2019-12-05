47087a02a4425cfeddc23c37917e055b4668f400
Coachmeister <jolun18@student.sdu.dk>
2019-12-05 13:11:13 +0100

    scd

--- Backend/code/main.py
+++ Backend/code/main.py
@@ -1,22 +1,27 @@
-from flask import Flask, redirect, url_for, request, jsonify
-from mysql.connector import (connection)
+from flask import Flask, redirect, url_for, request, jsonify, Response
+import mysql.connector
+
+
 app = Flask(__name__)
 
-cnx = connection.MySQLConnection(user='Jonas', password='password',
-                                 host='localhost',
-								database='people')
-cnx.close()
-
-
-@app.route('/person/', methods = ['POST'])
+@app.route('/person', methods = ['POST'])
 def post():
-	firstname = request.form['firstname']
-	lastname = request.form['lastname']
-	submit = request.form['submit']
+	Firstname = request.form['Firstname']
+	Lastname = request.form['Lastname']
+	cnx = mysql.connector.connect(user='root', password='',host='db',database='db')
+	mycursor = cnx.cursor()
+	sql = "INSERT INTO persons(Firstname, Lastname) VALUES (%s,%s)"
+	mycursor.execute(sql,(Firstname, Lastname))
+	cnx.commit()
+	return Response('', 200)
+	mycursor.close()
+	cnx.close()
 
 @app.route('/persons/', methods = ['GET'])
 def get():
 	return jsonify(PersonID = 0, firstname ="fuck", lastname = "you")
+	cnx = mysql.connector.connect(user='root', password='',host='db',database='db')
+
 
 
 if __name__ == '__main__':
---
+++ Database/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE db;
+use db;
+CREATE TABLE persons (
+   PersonID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   Firstname varchar(255) NOT NULL,
+   Lastname VARCHAR(255)
+);
